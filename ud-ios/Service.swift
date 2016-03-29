155616cd903990d2038f40679a58bcd600d93080
Gourave <gouravev@gmail.com>
2016-03-28 21:39:50 -0400

    Add services class

--- ud-ios/Service.swift
+++ ud-ios/Service.swift
@@ -2,4 +2,7 @@ import Foundation
 
 class Service {
     
+    let baseDefinitionUrl = "http://api.urbandictionary.com/v0/define"
+    let randomUrl = "http://api.urbandictionary.com/v0/random"
+    
 }
