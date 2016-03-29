bf8cfda1f9544a8785f07932d3a0cb14d5726287
Gourave <gouravev@gmail.com>
2016-03-28 22:06:01 -0400

    Add Alamofire pod

--- ud-ios/Service.swift
+++ ud-ios/Service.swift
@@ -1,8 +1,22 @@
 import Foundation
+import Alamofire
 
 class Service {
+    
+    
+    // MARK: Constants
     
     let baseDefinitionUrl = "http://api.urbandictionary.com/v0/define"
     let randomUrl = "http://api.urbandictionary.com/v0/random"
     
+    
+    // MARK: Methods
+    
+    public func getDefinition(word: String, completion: () -> ()) {
+        Alamofire.request(.GET, baseDefinitionUrl, parameters: ["term" : word]).responseJSON(completionHandler: {
+            response in
+            
+            completion()
+        })
+    }
 }
